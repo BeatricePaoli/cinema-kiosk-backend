288820652b91162cdb25faed58802edf27dc5fb0
Beatrice Paoli <beatrice.paoli@magentalab.it>
2023-08-10 23:59:55 +0200

    added seat chart to screen dto

--- src/main/java/com/example/cinemakiosk/dto/ScreenDto.java
+++ src/main/java/com/example/cinemakiosk/dto/ScreenDto.java
@@ -5,10 +5,13 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.beans.BeanUtils;
 
+import java.util.Map;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class ScreenDto extends RoomDto {
     private Integer totalSeats;
+    private Map<String, Object> seatChart;
 
     public static ScreenDto toDto(Screen screen) {
         ScreenDto dto = new ScreenDto();
