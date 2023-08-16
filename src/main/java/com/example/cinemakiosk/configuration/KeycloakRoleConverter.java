51942a938f161a7253d34c043d5340633ca8ab30
Beatrice Paoli <beatrice.paoli@magentalab.it>
2023-08-16 11:00:00 +0200

    added resource server config with keycloak

---
+++ src/main/java/com/example/cinemakiosk/configuration/KeycloakRoleConverter.java
@@ -0,0 +1,33 @@
+package com.example.cinemakiosk.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Value("${auth.client-id}")
+    private String clientId;
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        final Map<String, Object> resourceAccess = (Map<String, Object>) jwt.getClaims().get("resource_access");
+        final Map<String, Object> access = (Map<String, Object>) resourceAccess.get(clientId);
+        if (access != null) {
+            return ((List<String>)access.get("roles")).stream()
+                    .map(roleName -> "ROLE_" + roleName)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        } else {
+            return new ArrayList<>();
+        }
+    }
+}
---
+++ src/main/java/com/example/cinemakiosk/configuration/ResourceServerConfig.java
@@ -0,0 +1,66 @@
+package com.example.cinemakiosk.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Slf4j
+@Configuration
+public class ResourceServerConfig {
+
+    @Value("${cross-origins}")
+    String crossOrigins;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(getCorsConfig()))
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .ignoringRequestMatchers("api/movies")
+                )
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("api/movies", "api/movies/**").permitAll()
+                        .requestMatchers("api/shows", "api/shows/**").permitAll()
+                        .requestMatchers("api/theaters", "api/theaters/**").permitAll()
+                        .requestMatchers("api/bookings", "api/bookings/**").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    private CorsConfigurationSource getCorsConfig() {
+        log.info("Cross Origins setted to: {}", this.crossOrigins);
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList(crossOrigins));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/api/**", configuration);
+        return source;
+    }
+
+    private Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter() {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+        return jwtConverter;
+    }
+}
